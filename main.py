20db207b872c4e23a6d1e15f531b47789672afa4
Amy O <68756541+Sailormoon10@users.noreply.github.com>
2023-03-02 09:15:46 -0800

    Updated calculator with biweekly schedule

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # Date is automatically set up
 from datetime import date
 import semimonthly_schedule
+import biweekly_schedule
+
 
 def calculator_401k():
+    start_date = date(2023, 1, 6)
     today = date.today()
     current_day = today.today().day
     current_week = today.isocalendar()[1]
@@ -13,48 +16,52 @@ def calculator_401k():
     biweekly = 26
     weekly = 52
 
-    # current_biweekly_period = ??
+    current_biweekly_period = biweekly_schedule.biweekly_check(start_date, today)
     semimonthly_period = semimonthly_schedule.paycheck(current_month, current_day)
 
 
     remaining_months = monthly - current_month
-    remaining_weeks = weekly - current_week
-    remaining_biweekly_period = biweekly - 4
     remaining_semimonthly_period = semimonthly - semimonthly_period
+    remaining_biweekly_period = biweekly - current_biweekly_period
+    remaining_weeks = weekly - current_week
 
 
     def progress(frequency, goal, contribution):
         if frequency == "monthly":
             amount_needed = (goal - contribution) / remaining_months
+        elif frequency == "semimonthly":
+            amount_needed = (goal - contribution) / remaining_semimonthly_period
         elif frequency == "biweekly":
             amount_needed = (goal - contribution) / remaining_biweekly_period
-        else:
+        elif frequency == "weekly":
             amount_needed = (goal - contribution) / remaining_weeks
-        print(f"You need to save ${amount_needed:.2f} {frequency} by the end of the year to max your 401k.")
+        print(f'You need to save ${amount_needed:.2f} {frequency} by the end of the year to max your 401k.')
 
     # Function: User enters their pay schedule [Weekly, Biweekly, Monthly]
     # calculates the remaining weeks/pay periods to reach goal
     # and shows user how much they need to save to reach their goal by December 31st.
-    
     def pay_period(pay_schedule):
         pay_schedule.lower()
         if pay_schedule == "monthly":
             print(f'You have {remaining_months} months remaining to reach your 401k goal.')
+        elif pay_schedule == "semimonthly":
+            print(f'You have {remaining_semimonthly_period} pay periods remaining to reach your 401k goal.')
         elif pay_schedule == "biweekly":
             print(f'You have {remaining_biweekly_period} pay periods remaining to reach your 401k goal.')
         else:
             print(f"You have {remaining_weeks} weeks remaining to reach your 401k goal.")
 
-    # User enters retirement goal for the year
+    # User enters retirement goal for the year.
     goal = int(input("How much do you want to contribute to your 401k by the end of the year? $"))
 
     # User enters amount of their current cash contributions [exclude any investment gains]
     contribution = float(input("\nEnter the amount you have saved,\n[Exclude gains or employer match]: $"))
 
-    User enters their pay schedule [Weekly, Biweekly, Monthly]
-    pay_frequency = input("\nEnter how often you get paid (monthly, biweekly, weekly): ")
+    # User enters their pay schedule [Weekly, Biweekly, Monthly, Semimonthly]
+    pay_frequency = input("\nEnter how often you get paid (monthly, semimonthly, biweekly, weekly): ")
     pay_period(pay_frequency)
     progress(pay_frequency, goal, contribution)
 
 
 calculator_401k()
+
