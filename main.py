8a38ad2d6b7b97ce7d62a8a6e155932e02dcb4da
Amy O <68756541+Sailormoon10@users.noreply.github.com>
2023-03-08 19:33:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import date
 import semimonthly_schedule
 import biweekly_schedule
 
+# Use datetime and isocalender functions to retrieve the first paycheck date, and today's day, week, and month.
 
 def calculator_401k():
     start_date = date(2023, 1, 6)
@@ -10,22 +11,26 @@ def calculator_401k():
     current_day = today.today().day
     current_week = today.isocalendar()[1]
     current_month = today.month
-
+    
+# Assign the total number of checks that the user will receive in a calendar year according to their pay schedule.
     monthly = 12
     semimonthly = 24
     biweekly = 26
     weekly = 52
 
+# Determine the current pay period number for the semimonthly and biweekly pay schedule.
+
     current_biweekly_period = biweekly_schedule.biweekly_check(start_date, today)
     semimonthly_period = semimonthly_schedule.paycheck(current_month, current_day)
 
+# Determine the remaining amount of checks for each pay schedule.
 
     remaining_months = monthly - current_month
     remaining_semimonthly_period = semimonthly - semimonthly_period
     remaining_biweekly_period = biweekly - current_biweekly_period
     remaining_weeks = weekly - current_week
 
-
+# Calculate the amount you need to save depending on current contributions.
     def progress(frequency, goal, contribution):
         if frequency == "monthly":
             amount_needed = (goal - contribution) / remaining_months
